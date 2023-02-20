5783e7d6ae0286b68f2da87c161560b626f2ba61
Hai-Peter <55395655+Hai-Peter@users.noreply.github.com>
2023-02-20 19:38:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 MODE = os.environ.get('MODE', 'prod')
 WEB_HOOK = os.environ["WEB_HOOK"]
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ["5828213778:AAEcT5NlixzsK-8yWzG4XcNxu05A55ysB6k"]
 PORT = int(os.environ.get('PORT', '8080'))
 NICK = os.environ.get('NICK', None)
 config = {
-    "email": os.environ["EMAIL"],
-    "password": os.environ["PASSWORD"]
+    "email": os.environ["duckhai23@proton.me"],
+    "password": os.environ["haind2355"]
 }
 # deploy.sh config.py main.yml README.md docker-compose.yml github-action-secrets
